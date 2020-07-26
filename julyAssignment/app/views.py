0330751b2d14d854346d4de6cc707e51c870dab7
glossyyoon <mypju@naver.com>
2020-07-26 21:42:54 +0900

    검색창 만들기

---
+++ julyAssignment/app/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Blog
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.db.models import Q
+from django.http import HttpResponseRedirect
+from django.db.models import Q
+# Create your views here.
+class index(ListView):
+    template_name = 'index.html'
+    context_object_name = 'blog_list'
+    def get_queryset(self):
+        return Blog.objects.all
+
+class search(ListView):
+    template_name = 'search.html'
+    context_object_name = 'search_list'
+    def get_queryset(self):
+        return Blog.objects.all
+
+class detail(DetailView):
+    model = Blog
+    template_name = 'detail.html'
+    context_object_name = 'blog'
+
+class delete(DeleteView):
+    model = Blog
+    template_name = 'delete.html'
+    context_object_name = 'blog'
+    success_url = reverse_lazy('index')
+
+class update(UpdateView):
+    model = Blog
+    template_name = 'update.html'
+    fields = ['title', 'text']
+    success_url = reverse_lazy('index')
+
+def result(request):
+    BlogPosts = Blog.objects.all()
+
+    query = request.GET.get('query', '')
+    selected = request.GET.get('selected', '')
+    
+    if selected=="none":
+        BlogPosts = BlogPosts.filter(Q(title__icontains=query)| Q(text__icontains=query)).order_by('-time')
+        count = len(BlogPosts)
+        
+    elif selected=="title":
+        BlogPosts = BlogPosts.filter(Q(title__icontains=query)).order_by('-time')
+        count = len(BlogPosts)
+        
+    elif selected=="text":
+        BlogPosts = BlogPosts.filter(Q(text__icontains=query)).order_by('-time')
+        count = len(BlogPosts)
+        
+    elif selected=="author":
+        BlogPosts = BlogPosts.filter(Q(author__icontains=query)).order_by('-time')
+        count = len(BlogPosts)
+    return render(request, 'result.html', {'BlogPosts':BlogPosts, 'query':query, 'count':count, 'selected':selected})
+
+class create(CreateView):
+    model = Blog
+    template_name = 'create.html'
+    fields = ['title', 'text']
+    
+    def form_valid(self, form):
+        Blog = form.save(commit=False)
+        Blog.author  = self.request.user
+        Blog.save()
+
+        return HttpResponseRedirect(self.request.POST.get('next', '/'))
